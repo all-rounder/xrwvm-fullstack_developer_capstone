b8933eede745d9b02557a455cd0b5873eb362a0d
all-rounder <xiaoyao160@gmail.com>
2025-01-08 21:42:49 +1100

    linting

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,9 +14,9 @@ urlpatterns = [
 
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
-    
+
     # path for get cars
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 
     # path for dealers view
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
